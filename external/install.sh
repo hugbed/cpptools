74b42b2916377c0ad0aaab2e3d571d70c062103a
Hugo Bedard <Hugo.Bedard@USherbrooke.ca>
2017-02-06 10:53:02 -0500

    Install dependencies script

---
+++ external/install.sh
@@ -0,0 +1,19 @@
+# Create dependencies folder
+mkdir dependencies
+cd dependencies
+DEPENDENCIES_DIR=$PWD
+
+# params: github_url, repo_name
+function install {
+    git clone $1 $2 && \
+        cd $2 && \
+        mkdir build && \
+        cd build && \
+        cmake .. && \
+        make .. && \
+        make install
+    cd DEPENDENCIES_DIR 
+}
+
+install https://github.com/google/googletest.git googletest
+
